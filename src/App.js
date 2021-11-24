997c60324e9b9d08b56b1b525d51562df1b3c2d9
Nikolai Nanev <niki4etooo@gmail.com>
2021-11-24 09:32:17 +0200

    feat: portfolio with react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import React from "react";
 
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Testimonials from './components/Testimonials';
+
+
 export default function App() {
-  return "App";
+  return (
+	<main className="text-grey-400 bg-gray-900 body-font">
+		<Navbar />
+		<About />
+		<Projects />
+		<Skills />
+		<Testimonials />
+		<Contact />
+	</main>
+  );
 }
--- src/data.js
+++ src/data.js
@@ -1,49 +1,49 @@
 export const projects = [
   {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
+    title: "Coffee Time v2.0",
+    subtitle: "Coffee Time project with React and CSS animations",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+      "It is a simple project for people who likes coffee drinks and short description about each coffee.",
+    image: "./coffee-time.png",
+    link: "https://github.com/niki4etoo/coffee-time-v2.0.git",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
+    title: "Example Text Animations",
+    subtitle: "Example Text Animations with React, styled-components and CSS animations",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+      "Very short project with text animations paragraphs and titles. Example usage of styled-components, CSS animations and transitions",
+    image: "./text-animation.gif",
+    link: "https://github.com/niki4etoo/example-text-animation.git",
   },
   {
-    title: "DevChat",
-    subtitle: "React and Firebase",
+    title: "Example Box Animations",
+    subtitle: "Example Box Animations with React, styled-components and CSS animations",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
+      "It is almost the same project as text animation, but instead of text there are several boxes and applied animation on them.",
+    image: "./box-animation.gif",
+    link: "https://github.com/niki4etoo/example-box-animation.git",
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
+    title: "Coffee Shop",
+    subtitle: "Coffee shop is a web online shop",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+      "Coffee shop is a web online shop for buying different coffee drinks. Now it is under development.",
+    image: "./coffee-time.png",
+    link: "https://github.com/niki4etoo/coffee-shop.git",
   },
 ];
 
 export const testimonials = [
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+      "this app is great! simple easy to use and navigate around, if your a barista or just want to brew great coffee at home i definitely recommend giving it a go, it has guides on how to brew from a wide range of coffee making equipment even roasterys that roast there own beans are posting guides on how to brew there coffee on chemex v60 and more! The developer is good at responding to any feedback and im glad he has brought this app over to React, thankyou",
     image: "https://randomuser.me/api/portraits/men/1.jpg",
     name: "Jesse Hicks",
     company: "Zoozle",
   },
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+      "FANTASTIC APP!! I use this all the time for brewing at home and at work and some of my team use this also. For me this app would be 5 stars if the built in timer for pouring water was more accurate to a real life pour. I know this is quite hard to achieve in an app that isnt connected to scales. My rate of pouring is a lot slower compared to the app!! Other then thay fantastic app. So easy to use. Super easy to store recipes and brew history and it looks great!! Maybe add a fetco option!!",
     image: "https://randomuser.me/api/portraits/men/94.jpg",
     name: "Ruben Alvarez",
     company: "Dooble.io",
@@ -55,6 +55,6 @@ export const skills = [
   "React",
   "Next.js",
   "Node",
-  "GraphQL",
-  "Material UI",
+  "HTML&CSS",
+  "Git",
 ];
