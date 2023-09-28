a47103bd0d2edc3dd544287343d8ad4d0c3f1fd8
Nikolai Nanev <niki4etooo@gmail.com>
2023-09-28 09:13:19 +0300

    added for each answer an description and removing question when clicked on some answer

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,10 @@ function Header(){
 
     const [simplicityAnswers, setSimplicityAnswers] = useState(false);
 
+    const [answerDescriptionYes, setAnswerDescriptionYes] = useState(false);
+    const [answerDescriptionNo, setAnswerDescriptionNo] = useState(false);
+    const [answerDescriptionItDepends, setAnswerDescriptionItDepends] = useState(false);
+
 
     const showQuestion = () => {
         setSimplicityQuestion(prev => !prev);
@@ -17,27 +21,85 @@ function Header(){
         setSimplicityAnswers(prev => !prev);
     }
 
+    // Description for each answer (on/off)
+    const showDescriptionYes = () => {
+        setAnswerDescriptionYes(true);
+        setSimplicityAnswers(false);
+        setSimplicityQuestion(false);
+
+        const portfolio = document.querySelector("#portfolio");
+        portfolio.style.pointerEvents = "none";
+
+        //To do ( navigate to Yes component )
+
+    }
+
+    const showDescriptionNo = () => {
+        setAnswerDescriptionNo(true);
+        setSimplicityAnswers(false);
+        setSimplicityQuestion(false);
+
+        const portfolio = document.querySelector("#portfolio");
+        portfolio.style.pointerEvents = "none";
+
+        //To do ( navigate to No component )
+    }
+
+    const showDescriptionItDepends = () => {
+        setAnswerDescriptionItDepends(true);
+        setSimplicityAnswers(false);
+        setSimplicityQuestion(false);
+
+        const portfolio = document.querySelector("#portfolio");
+        portfolio.style.pointerEvents = "none";
+
+        //To do ( navigate to It Depends component )
+    }
+
+
     return (
         <>
         <div className='header'>
-            <h1 onClick={showQuestion}>Portfolio</h1>
+            <h1 id='portfolio' onClick={showQuestion}><span id='blue'>Port</span>folio</h1>
         </div>
         { 
             simplicityQuestion &&
 
             <div className='question'>
-                <h2 onClick={showAnswers}>Is simplicity important?</h2>
+                <h1 onClick={showAnswers}>Is simplicity <span id='blue'>important</span>?</h1>
             </div>
         }
         {
             simplicityAnswers && 
 
             <div className='answers'>
-                <button onClick={() => { console.log("Go to Yes component(to do)");}}>Yes</button>
-                <button onClick={() => { console.log("Go to No component(to do)");}}>No</button>
-                <button onClick={() => { console.log("Go to It depends component(to do)");}}>It depends</button>
+                <button onClick={showDescriptionYes}>Yes</button>
+                <button onClick={showDescriptionNo}>No</button>
+                <button onClick={showDescriptionItDepends}>It depends</button>
             </div>
         }
+        <div className='answersDescription'>
+        {
+            answerDescriptionYes &&
+
+                <div id='yes'>
+                    Answer description for the "YES"
+                </div>
+
+        }
+        {
+            answerDescriptionNo &&
+            <div id='no'>
+                    Answer description for the "No"
+                </div>
+        }
+        {
+                answerDescriptionItDepends &&
+                <div id='itDepends'>
+                    Answer description for the "It Depends"
+                </div>
+        }
+        </div>
         </>
         
     );
