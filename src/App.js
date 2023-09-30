8b5f23ba9242fa58d332e39179d9dfdc68abd195
Nikolai Nanev <niki4etooo@gmail.com>
2023-09-30 06:25:39 +0300

    added react-router-dom and navigation to the three components(yes, no and depends)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 
+import {Routes, Route, useLocation } from 'react-router-dom';
+
 import Header from './components/Header';
 
+import YesAnswer from './components/YesAnswer';
+import NoAnswer from './components/NoAnswer';
+import ItDependsAnswer from './components/ItDependsAnswer';
+
 function App() {
+
+  const location = useLocation();
+
   return (
     <>
-    <Header />
+    <Routes location={location} key={location.key}>
+				<Route path='/*' element={<Header />}></Route>
+				<Route path='/yes' element={<YesAnswer />}></Route>
+				<Route path='/no' element={<NoAnswer />}></Route>
+        <Route path='/itDepends' element={<ItDependsAnswer />}></Route>
+			</Routes>
     </>
   );
 }
