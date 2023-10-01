237b25422052e742ac05a31ef084563b3986e96a
Nikolai Nanev <niki4etooo@gmail.com>
2023-10-01 06:06:42 +0300

    added an example question with answers

--- src/components/YesAnswer.js
+++ src/components/YesAnswer.js
@@ -2,10 +2,53 @@ import React from "react";
 
 import Navigation from "./Navigation";
 
+import '../styles/quiz.css';
+
 const YesAnswer = () => {
+
+    const handleToBeLived = () => {
+        console.log(`Here we go! Excellent`);
+    }
+
+    const handleToBeUnderstood = () => {
+        console.log(`Very good! Wisdom Seeker`);
+    }
+
+    const handleIDoNotKnow = () => {
+        console.log(`You should know. Find the answers`);
+    }
+
+    const handleToGetRicher = () => {
+        console.log(`The life is one of the best teachers, and yes, you get richer`);
+    }
+
     return (
         <>
         <Navigation />
+        <div className="yes-quiz-container">
+            <div className="yes-quiz-question">
+                What is the purpose of life?
+            </div>
+
+            <div className="yes-quiz-answers">
+                <div className="yes-quiz-answer" onClick={handleToBeLived}>
+                    To be lived
+                </div>
+
+                <div className="yes-quiz-answer" onClick={handleToBeUnderstood}>
+                    To be underdstood
+                </div>
+
+                <div className="yes-quiz-answer" onClick={handleIDoNotKnow}>
+                    I don't know
+                </div>
+
+                <div className="yes-quiz-answer" onClick={handleToGetRicher}>
+                    To get richer
+                </div>
+            </div>
+            
+        </div>
         </>
     );
 }
