0b65c1d6ceefa0aa007097acab6a1deabe64ffda
Nikolai Nanev <niki4etooo@gmail.com>
2023-10-02 09:21:01 +0300

    added translateY and opacity effect on the portfolio title and removed redundant code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
+import { gsap } from 'gsap';
 import '../styles/header.css';
 
 
 const Header = () => {
+
+    const portfolioRef = useRef(null);
 
     const [simplicityQuestion, setSimplicityQuestion] = useState(false);
     const [simplicityAnswers, setSimplicityAnswers] = useState(false);
@@ -13,8 +16,23 @@ const Header = () => {
     const [answerDescriptionItDepends, setAnswerDescriptionItDepends] = useState(false);
 
     const showQuestion = () => {
-        setSimplicityQuestion(prev => !prev);
-        setSimplicityAnswers(false);
+
+        let mainTimeline = gsap.timeline();
+
+        mainTimeline.to(portfolioRef.current, {
+            y: -300,
+            opacity: 0,
+            ease: "Power4.easeOut"
+        }).to('.header', {
+            height: "10vh",
+            stagger: 1.5,
+            onComplete: () => { 
+                setSimplicityQuestion(prev => !prev);
+                setSimplicityAnswers(false);
+            }
+        });
+
+        
     }
 
     const showAnswers = () => {
@@ -22,92 +40,69 @@ const Header = () => {
     }
 
     // Description for each answer (on/off)
-    const showDescriptionYes = () => {
-        setAnswerDescriptionYes(true);
+
+    const showDescription = (answer) => {
+        if(answer === "yes"){
+            setAnswerDescriptionYes(true);
+        } else if(answer === "no"){
+            setAnswerDescriptionNo(true);
+        } else if(answer === "itDepends"){
+            setAnswerDescriptionItDepends(true);
+        }
         setSimplicityAnswers(false);
         setSimplicityQuestion(false);
-
-        const portfolio = document.querySelector("#portfolio");
-        portfolio.style.pointerEvents = "none";
-
-        //To do ( navigate to Yes component )
-
-    }
-
-    const showDescriptionNo = () => {
-        setAnswerDescriptionNo(true);
-        setSimplicityAnswers(false);
-        setSimplicityQuestion(false);
-
-        const portfolio = document.querySelector("#portfolio");
-        portfolio.style.pointerEvents = "none";
-
-        //To do ( navigate to No component )
-    }
-
-    const showDescriptionItDepends = () => {
-        setAnswerDescriptionItDepends(true);
-        setSimplicityAnswers(false);
-        setSimplicityQuestion(false);
-
-        const portfolio = document.querySelector("#portfolio");
-        portfolio.style.pointerEvents = "none";
-
-        //To do ( navigate to It Depends component )
     }
 
 
     return (
-        <>
-        <div className='header'>
-            <h1 id='portfolio' onClick={showQuestion}><span id='blue'>Port</span>folio</h1>
-        </div>
-        { 
-            simplicityQuestion &&
-                <div className='questions'>
-                    <h1 onClick={showAnswers}>Is simplicity <span id='blue'>important</span>?</h1>
-                </div>
-            
-        }
-        {
-            simplicityAnswers && 
-
-            <div className='answers'>
-                <button onClick={showDescriptionYes}>Yes</button>
-                <button onClick={showDescriptionNo}>No</button>
-                <button onClick={showDescriptionItDepends}>It depends</button>
+        <div className='container'>
+            <div className='header'>
+                <h1 ref={portfolioRef} onClick={() => showQuestion()}><span id='blue'>Port</span>folio</h1>
             </div>
-        }
-        <div className='answersDescription'>
-        {
-            answerDescriptionYes &&
+            { 
+                simplicityQuestion &&
+                    <div className='questions'>
+                        <h1 onClick={showAnswers}>Is simplicity <span id='blue'>important</span>?</h1>
+                    </div>
+            }
+            {
+                simplicityAnswers && 
 
-                <div className='answer-description'>
-                    <h1>YES</h1>
-                    <h3>The correct answer is "yeah". Why? Because makes <span id='blue'>life</span> easier and actually richer.</h3>
-                    <Link to='/yes'>Learn More</Link>
+                <div className='answers'>
+                    <button onClick={() => showDescription("yes")}>Yes</button>
+                    <button onClick={() => showDescription("no")}>No</button>
+                    <button onClick={() => showDescription("itDepends")}>It depends</button>
                 </div>
+            }
+            <div className='answersDescription'>
+            {
+                answerDescriptionYes &&
+                    <div className='answer-description'>
+                        <h1>YES</h1>
+                        <h3>The correct answer is "yeah". Why? Because makes <span id='blue'>life</span> easier and actually richer.</h3>
+                        <Link to='/yes'>Learn More</Link>
+                    </div>
 
-        }
-        {
-            answerDescriptionNo &&
-                <div className='answer-description'>
-                    <h1>NO</h1>
-                    <h3>This is not the correct answer, but anyway. Maybe complicated is your <span id='blue'>type</span>, which in some cases is better.</h3>
-                    <Link to='/no'>Learn More</Link>
-                </div>
-        }
-        {
+            }
+            {
+                answerDescriptionNo &&
+                    <div className='answer-description'>
+                        <h1>NO</h1>
+                        <h3>This is not the correct answer, but anyway. Maybe complicated is your <span id='blue'>type</span>, which in some cases is better.</h3>
+                        <Link to='/no'>Learn More</Link>
+                    </div>
+            }
+            {
                 answerDescriptionItDepends &&
-                <div className='answer-description'>
-                    <h1>It Depends</h1>
-                    <h3>It depends on what? Do you hesitate for something? Are you sure? 
-                        It's okay, you can find answers for the <span id='blue'>simplicity</span> in some books or kind of books.</h3>
-                        <Link to='/itDepends'>Learn More</Link>
-                </div>
-        }
+                    <div className='answer-description'>
+                        <h1>It Depends</h1>
+                        <h3>It depends on what? Do you hesitate for something? Are you sure? 
+                            It's okay, you can find answers for the <span id='blue'>simplicity</span> in some books or kind of books.</h3>
+                            <Link to='/itDepends'>Learn More</Link>
+                    </div>
+            }
+            </div>
         </div>
-        </>
         
     );
 }
