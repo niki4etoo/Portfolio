1f4937abcbd77db89acfa49c71672d8068f6acf0
Nikolai Nanev <niki4etooo@gmail.com>
2023-10-07 07:36:16 +0300

    added list of answers( roman ) and nav to start again

--- src/components/AnsweredQuestions.js
+++ src/components/AnsweredQuestions.js
@@ -1,19 +1,26 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import '../styles/quiz.css';
 
 
-const AnsweredQuestions = (userAnswers) => {
+const AnsweredQuestions = (userAnswers, page) => {
     return (
-        <>
+        <div className="answer-list">
             <div className="user-answer-title">Your answers</div>
-            {
-                userAnswers.questions.map((a, k) => {
-                    return (
-                        <div className="user-answer" key={k}>{k+1}.&nbsp;<span id="blue">{a}</span></div>
-                    )
-                })
-            }
-        </>
+            <ol className="roman">
+                {
+                    userAnswers.questions.map((a, k) => {
+                        return (
+                            <li key={k}>&nbsp;{a}</li>
+                        )
+                    })
+                }
+            </ol>
+            <div className="back-again">
+                <Link to={page}>Again?</Link>
+            </div>
+            
+        </div>
     );
 }
 
