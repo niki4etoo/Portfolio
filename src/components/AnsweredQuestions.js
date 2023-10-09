6b83da60a1aa19820eaf034fcc1f3bc059d534d0
Nikolai Nanev <niki4etooo@gmail.com>
2023-10-09 08:03:53 +0300

    removed unused list of strings for answered times

--- src/components/AnsweredQuestions.js
+++ src/components/AnsweredQuestions.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-import times from '../Decisions/answeredCounter.json';
-
 import '../styles/quiz.css';
 
 
@@ -11,7 +9,6 @@ const AnsweredQuestions = (userAnswers, page) => {
     return (
         <div className="answer-list">
             <div className="user-answer-title">Your answers</div>
-            <div className="user-answer-times">{times.list[0]}</div>
             <ol className="roman">
                 {
                     userAnswers.questions.map((a, k) => {
