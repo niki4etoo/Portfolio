baad70dfd270dbb82f3596ac577085e14832264b
Nikolai Nanev <niki4etooo@gmail.com>
2023-10-16 08:54:18 +0300

    Added select options, buttons, styling and translation files for questions component

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 
 //Languages
-import bg from '../languages/bg.json';
-import en from '../languages/en.json';
+
+import bg from '../languages/bg-questions.json';
+import en from '../languages/en-questions.json';
 
 import Navigation from "./Navigation/Navigation";
 import Menu from "./Menu";
@@ -14,49 +15,78 @@ import '../styles/questions.css';
 const Questions = (props) => {
 
     //Languages ( BG | EN)
-    const { state } = useLocation();
+    let { state } = useLocation();
 
-    const [currentLanguage, setCurrentLanguage] = useState(state?.lang);
+    const [currentLanguage, setCurrentLanguage] = useState(state?.lang || false);
 
     const changeLanguage = (e) => {
         if (e.target.checked) {
             setCurrentLanguage(prev => true); //Switched to EN
+            console.log("1. Current language: ", currentLanguage);
         } else {
             setCurrentLanguage(prev => false); //Switched to BG
+            console.log("1. Current language: ", currentLanguage);
         }
     }
 
     let l = {};
     (currentLanguage) ? l = en : l = bg;
+    const Difficulty = (props) => {
+        let l = {};
+        (props.lang) ? l = en : l = bg;
+
+        return (
+            <>
+                <select name="difficulty" className="difficulty">
+                    <option value="Easy">{l.questions.select.easy}</option>
+                    <option value="Medium">{l.questions.select.medium}</option>
+                    <option value="Hard">{l.questions.select.hard}</option>
+                </select>
+            </>
+        );
+    }
+    
+    const Buttons = (props) => {
+        let l = {};
+        (props.lang) ? l = en : l = bg;
+    
+        return (
+            <>
+                <button className="buttons" type="button">{l.questions.buttons.status}</button>
+                <button className="buttons" type="button">{l.questions.buttons.start}</button>
+            </>
+        );
+    }
+    
 
     return (
         <>
             <Navigation lang={currentLanguage} />
             <div className="border">
                 <div className="container__questions">
-                    <div className="cell__questions header__questions">Category</div>
-                    <div className="cell__questions header__questions">Difficulty</div>
-                    <div className="cell__questions header__questions">Options</div>
+                    <div className="cell__questions header__questions">{l.questions.category.title}</div>
+                    <div className="cell__questions header__questions">{l.questions.difficulty}</div>
+                    <div className="cell__questions header__questions">{l.questions.options}</div>
 
                     {/* Technical questions about the stack */}
-                    <div className="cell__questions">Technical</div>
-                    <div className="cell__questions">Easy/Medium/Hard</div>
-                    <div className="cell__questions">Status/Start</div>
+                    <div className="cell__questions">{l.questions.category.technical}</div>
+                    <div className="cell__questions"><Difficulty lang={currentLanguage} /></div>
+                    <div className="cell__questions"><Buttons lang={currentLanguage} /></div>
 
                     {/* Personal questions about self-aware and purpose driven */}
-                    <div className="cell__questions">Personal</div>
-                    <div className="cell__questions">Easy/Medium/Hard</div>
-                    <div className="cell__questions">Status/Start</div>
+                    <div className="cell__questions">{l.questions.category.personal}</div>
+                    <div className="cell__questions"><Difficulty lang={currentLanguage} /></div>
+                    <div className="cell__questions"><Buttons lang={currentLanguage} /></div>
 
                     {/* Work questions about purpose, time, money */}
-                    <div className="cell__questions">Work</div>
-                    <div className="cell__questions">Easy/Medium/Hard</div>
-                    <div className="cell__questions">Status/Start</div>
+                    <div className="cell__questions">{l.questions.category.work}</div>
+                    <div className="cell__questions"><Difficulty lang={currentLanguage} /></div>
+                    <div className="cell__questions"><Buttons lang={currentLanguage} /></div>
 
                     {/* Entertainment questions about fun, chill-out and better understanding */}
-                    <div className="cell__questions">Entertainment</div>
-                    <div className="cell__questions">Easy/Medium/Hard</div>
-                    <div className="cell__questions">Status/Start</div>
+                    <div className="cell__questions">{l.questions.category.entertainment}</div>
+                    <div className="cell__questions"><Difficulty lang={currentLanguage} /></div>
+                    <div className="cell__questions"><Buttons lang={currentLanguage} /></div>
                 </div>
             </div>
             <Menu lang={currentLanguage} />
