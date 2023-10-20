03c909299e2aeb52f38e5bf8f48fe7cf1b684cf1
Nikolai Nanev <niki4etooo@gmail.com>
2023-10-20 07:42:15 +0300

    todo: sending to email

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -41,7 +41,8 @@ const Contacts = (props) => {
             [e.target.name]: val});
     }
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
         console.log("Name: ", form.name);
         console.log("Mail: ", form.mail);
         console.log("Message: ", form.message); // to do (send to server)
@@ -54,21 +55,25 @@ const Contacts = (props) => {
             <div className="contacts-container">
                 <div className="contacts"><h1>{l.contacts.title}</h1></div>
                 <div className="contacts-form">
-                    <form id="contact" onSubmit={handleSubmit}>
+                    <form id="contact" method="post" onSubmit={handleSubmit}>
                         <div className="contacts-input">
                             <fieldset className="contacts-item">
-                                <input placeholder={l.contacts.form.placeholders.name} id="name" name="name" value={form.name} type="text" tabIndex="1" onChange={handleChange} />
+                                <input placeholder={l.contacts.form.placeholders.name} maxLength={15} id="name" name="name" value={form.name} type="text" tabIndex="1" onChange={handleChange} required />
                             </fieldset>
                             <fieldset className="contacts-item">
-                                <input placeholder={l.contacts.form.placeholders.mail} name="mail" value={form.mail} type="email" tabIndex="2" onChange={handleChange} />
+                                <input placeholder={l.contacts.form.placeholders.mail} name="mail" value={form.mail} type="email" tabIndex="2" onChange={handleChange} required />
                             </fieldset>
                         </div>
                         <fieldset className="contacts-item">
-                            <textarea placeholder={l.contacts.form.placeholders.message} name="message" value={form.message} tabIndex="3" onChange={handleChange}></textarea>
+                            <textarea placeholder={l.contacts.form.placeholders.message} maxLength={450} name="message" value={form.message} tabIndex="3" onChange={handleChange} required></textarea>
                         </fieldset>
                         <div className="contacts-buttons">
                             <fieldset className="contacts-item">
-                                <button name="reset" type="reset" id="contact-reset">{l.contacts.form.buttons.reset}</button>
+                                <button name="reset" type="reset" id="contact-reset" onClick={(e) => { setForm({
+                                    name: "",
+                                    mail: "",
+                                    message: "",
+                                }); }}>{l.contacts.form.buttons.reset}</button>
                             </fieldset>
                             <fieldset className="contacts-item">
                                 <button type="submit" id="contact-submit" onClick={handleSubmit}>{l.contacts.form.buttons.send}</button>
