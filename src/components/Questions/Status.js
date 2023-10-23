968128c42c1b0b32716c6ba900035b0e3774625e
Nikolai Nanev <niki4etooo@gmail.com>
2023-10-23 06:10:43 +0300

    added Status component ( to do )

---
+++ src/components/Questions/Status.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const Status = (props) => {
+
+    [ qStatus, setqStatus ] = useState({});
+
+    return (
+        <>
+        
+        </>
+    );
+}
+
+export default Status;
