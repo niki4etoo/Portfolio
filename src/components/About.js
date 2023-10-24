32b8017a12a9f8a067ef496b2c871fa871058186
Nikolai Nanev <niki4etooo@gmail.com>
2023-10-24 05:51:37 +0300

    styling and changed position of tech stack in about component

--- src/components/About.js
+++ src/components/About.js
@@ -40,7 +40,7 @@ const About = () => {
                 <div className="about"><h1>{l.about.title}</h1></div>
                 <section className="profile">
                     <figure>
-                        <a href="https://github.com/niki4etoo/"><img src={profileQuestion} width={200} height={200} alt="Profile"/></a>
+                        <a href="https://github.com/niki4etoo/"><img src={profileQuestion} width={200} height={200} alt="Profile" /></a>
                     </figure>
                 </section>
                 <section className="intro">
@@ -48,18 +48,24 @@ const About = () => {
                 </section>
                 <section className="tech-stack">
                     <h1>{l.about.techStack}</h1>
-                    <h2>{l.about.tech.used}</h2>
-                    <ul>
-                        <li><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">{l.about.tech.js}</a></li>
-                        <li><a href="https://developer.mozilla.org/en-US/docs/Web/HTML">{l.about.tech.html}</a> & <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">{l.about.tech.css}</a></li>
-                        <li><a href="https://react.dev/">{l.about.tech.react}</a></li>
-                    </ul>
-                    <h2>{l.about.tech.learning}</h2>
-                    <ul>
-                        <li><a href="https://gsap.com/">{l.about.tech.gsap}</a></li>
-                        <li><a href="https://www.react-spring.dev/">{l.about.tech.reactSpring}</a></li>
-                        <li><a href="https://nodejs.org/en">{l.about.tech.node}</a></li>
-                    </ul>
+                    <div className="tech-stack-row">
+                        <div className="tech-stack-column">
+                            <h2>{l.about.tech.used}</h2>
+                            <ul>
+                                <li><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">{l.about.tech.js}</a></li>
+                                <li><a href="https://developer.mozilla.org/en-US/docs/Web/HTML">{l.about.tech.html}</a> & <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">{l.about.tech.css}</a></li>
+                                <li><a href="https://react.dev/">{l.about.tech.react}</a></li>
+                            </ul>
+                        </div>
+                        <div className="tech-stack-column">
+                            <h2>{l.about.tech.learning}</h2>
+                            <ul>
+                                <li><a href="https://gsap.com/">{l.about.tech.gsap}</a></li>
+                                <li><a href="https://www.react-spring.dev/">{l.about.tech.reactSpring}</a></li>
+                                <li><a href="https://nodejs.org/en">{l.about.tech.node}</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </section>
 
             </div>
