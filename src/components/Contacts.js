cb72dc590c1088bc3294483d44d68a6f41ae3451
Nikolai Nanev <niki4etooo@gmail.com>
2023-10-30 08:09:54 +0200

    changed contact form using mailto and added social links to contact component

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
+import linkedLogo from '../images/logo/linkedin.png';
+import githubLogo from '../images/logo/github.png';
+import twitterLogo from '../images/logo/twitter.png';
 
 //Languages
 import bg from '../languages/bg.json';
@@ -22,69 +26,33 @@ const Contacts = (props) => {
     let l = {};
     (currentLanguage) ? l = en : l = bg;
 
-    const [form, setForm] = useState({
-        name: "",
-        mail: "",
-        message: "",
-    });
-
-    const handleChange = (e) => {
-        const val = e.target.value;
-        setForm({
-            ...form,
-            [e.target.name]: val
-        });
+    const ButtonContact = ({ label, mailto }) => {
+        return (
+            <Link
+                to="#"
+                className="button-send__contacts"
+                onClick={(e) => {
+                    window.location.href = mailto;
+                    e.preventDefault();
+                }}
+            >
+                {label}
+            </Link>
+        );
     }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        const contactForm = document.querySelector("#contactForm");
-        const formData = new FormData(contactForm);
-        
-        fetch("/", {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: new URLSearchParams(formData).toString(),
-        })
-            .then(() => alert("/thank-you/"))
-            .catch((error) => alert(error));
-
-    };
 
     return (
         <>
             <Navigation lang={currentLanguage} />
-            <div className="contacts-container">
-                <div className="contacts"><h1>{l.contacts.title}</h1></div>
-                <div className="contacts-form">
-                    <form id="contactForm" method="post" onSubmit={handleSubmit} name="contact" data-netlify="true">
-                        <div className="contacts-input">
-                            <fieldset className="contacts-item">
-                                <input placeholder={l.contacts.form.placeholders.name} maxLength={15} id="name" name="name" value={form.name} type="text" tabIndex="1" onChange={handleChange} required />
-                            </fieldset>
-                            <fieldset className="contacts-item">
-                                <input placeholder={l.contacts.form.placeholders.mail} name="mail" value={form.mail} type="email" tabIndex="2" onChange={handleChange} required />
-                            </fieldset>
-                        </div>
-                        <fieldset className="contacts-item">
-                            <textarea placeholder={l.contacts.form.placeholders.message} maxLength={450} name="message" value={form.message} tabIndex="3" onChange={handleChange} required></textarea>
-                        </fieldset>
-                        <div className="contacts-buttons">
-                            <fieldset className="contacts-item">
-                                <button name="reset" type="reset" id="contact-reset" onClick={(e) => {
-                                    setForm({
-                                        name: "",
-                                        mail: "",
-                                        message: "",
-                                    });
-                                }}>{l.contacts.form.buttons.reset}</button>
-                            </fieldset>
-                            <fieldset className="contacts-item">
-                                <button type="submit" id="contact-submit">{l.contacts.form.buttons.send}</button>
-                            </fieldset>
-                        </div>
-                    </form>
+            <div className="contacts-container__contacts">
+                <div className="title__contacts"><h1>{l.contacts.title}</h1></div>
+                <div className="social-links__contacts">
+                <a href="https://www.linkedin.com/in/nikolay-nanev-75130275/"><img src={linkedLogo} alt="LinkedIn Logo" width="32" height="32" /></a>
+                <a href="https://twitter.com/niki4etooo/"><img src={twitterLogo} alt="Twitter Logo" width="32" height="32" /></a>
+                <a href="https://github.com/niki4etoo/"><img src={githubLogo} alt="Github Logo" width="32" height="32" /></a>
+                </div>
+                <div className="contact-button__contacts">
+                    <ButtonContact label={l.contacts.label} mailto="mailto:niki4etooo@gmail.com" />
                 </div>
             </div>
             <Menu lang={currentLanguage} />
