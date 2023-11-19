ff7188bcbc394f3b8b65bfa5214d2a73293dcd2d
Nikolai Nanev <niki4etooo@gmail.com>
2023-11-19 10:22:43 +0200

    added status start button

--- src/components/Questions/Status.tsx
+++ src/components/Questions/Status.tsx
@@ -1,6 +1,6 @@
 
 import { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import LanguageSwitch from '../LanguageSwitch';
 
@@ -23,17 +23,36 @@ const Status = (props: any) => {
     let l = currentLanguage ? en : bg;
 
     console.log(props);
+    console.log(state);
+    const navigate = useNavigate();
+
+    const Buttons = () => {
+
+        const handleStart = () => {
+            navigate(`/questions/${state.category}/`, { state: { ... state, lang: currentLanguage } });
+        }
+
+        return (
+            <>
+                <button className='buttons' type='button' onClick={handleStart}>{l.button.start}</button>
+            </>
+        );
+    }
 
     return (
         <>
             <Navigation lang={currentLanguage} navigate={'/questions'} />
             <div className='container__status'>
                 <div className='header__status'><h1>{l.title}</h1></div>
-                <div className='current__status'></div>
-                <div className='start__status'></div>
+                <div className='current__status'>
+
+                </div>
+                <div className='start__status'>
+                    <Buttons />
+                </div>
             </div>
-            
-            <LanguageSwitch lang={currentLanguage} setter={setCurrentLanguage} /> 
+
+            <LanguageSwitch lang={currentLanguage} setter={setCurrentLanguage} />
         </>
     );
 }
